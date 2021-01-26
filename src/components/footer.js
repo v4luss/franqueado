99d82210a245584d993aa8afade6a53e501a253d
v4luss <v4luss@gmail.com>
2021-01-26 19:00:14 -0300

    addedTextHover

--- src/components/footer.js
+++ src/components/footer.js
@@ -27,6 +27,7 @@ import GooglePlayBtn from '../images/google_play.png'
 import AppleStoreBtn from '../images/apple_store.png'
 import CellphoneIcon from '../images/cellicon.png'
 import TelephoneIcon from '../images/telephoneicon.png'
+import styles from '../styles/footer.module.css'
 
 const Section5 = () => {
     return (
@@ -36,59 +37,59 @@ const Section5 = () => {
                     <figure style={{}}>
                         <img src={LogoImg} style={{width: '200px'}}/>
                     </figure>
-                    <div style={{marginBottom: '20px'}}>
+                    <div style={{marginBottom: '20px', marginTop: '50px'}}>
                         <img src={CellphoneIcon} style={{width: '10px'}}/>
-                        <span style={{paddingTop: '20px'}}> &nbsp; (51)98126-1653</span>
+                        <span style={{paddingTop: '20px'}} className={styles.changeColor}> &nbsp; <a>(51)98126-1653</a></span>
                     </div>
                     <div>
                         <img src={TelephoneIcon} style={{width: '10px'}}/>
-                        <span>&nbsp; (51)3391-6340</span>
+                        <span className={styles.changeColor}> &nbsp; <a>(51)3391-6340</a></span>
                     </div>
                     <figure style={{position: 'relative', left: '-20px'}}>
-                        <img src={LogoFace} style={{width: '50px'}}/>
-                        <img src={LogoInsta} style={{width: '50px'}}/>
-                        <img src={LogoLink} style={{width: '70px'}}/>
-                        <img src={LogoWhats} style={{width: '30px'}}/>
-                        <img src={LogoYou} style={{width: '60px', paddingLeft: '20px'}}/>
+                        <img src={LogoFace} style={{width: '50px'}} className={styles.icon}/>
+                        <a className={styles.changeColor}><img src={LogoInsta} style={{width: '50px'}}/></a>
+                        <a><img src={LogoLink} style={{width: '70px'}}/></a>
+                        <a><img src={LogoWhats} style={{width: '30px'}}/></a>
+                        <a><img src={LogoYou} style={{width: '60px', paddingLeft: '20px'}}/></a>
                     </figure>
                 </div>
                 <div style={{float: 'left', paddingLeft: '80px'}}>
                     <ul>
                         <li style={{paddingBottom: '20px', listStyleType: 'none'}}>Módulos</li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Plataforma</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>CRM</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Arquivos</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>CAF</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Checklist</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Projetos</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Compras</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Plataforma</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>CRM</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Arquivos</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>CAF</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Checklist</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Projetos</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Compras</a></li>
                     </ul>
                 </div>
                 <div style={{float: 'left', paddingLeft: '30px'}}>
                     <ul>
                         <li style={{paddingBottom: '20px', listStyleType: 'none'}}>A Central</li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Seja um Parceiro</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Acesse a sua Conta</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Plataforma</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Planos e Preços</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Perguntas Frequentes</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Anuncie</a></li>
-                        <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Franquias</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Seja um Parceiro</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Acesse a sua Conta</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Plataforma</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Planos e Preços</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Perguntas Frequentes</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Anuncie</a></li>
+                        <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Franquias</a></li>
                     </ul>
                 </div>
                 <div style={{float: 'left', paddingLeft: '10px'}}>
                     <ul>
                     <li style={{paddingBottom: '20px', listStyleType: 'none'}}>Educativo</li>
-                    <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Cursos por E-mail</a></li>
-                    <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Blog</a></li>
+                    <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Cursos por E-mail</a></li>
+                    <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Blog</a></li>
                     </ul>
                 </div>
                 <div style={{float: 'left', width: '260px'}}>
                     <ul>
                     <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Últimas do Blog</a></li>
-                    <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>18 Franquias online para você trabalhar sem sair de casa</a></li>
-                    <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Descubra como cobrar Royalties em uma rede de franquias</a></li>
-                    <li style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Franquias baratas: mais de 100 dicas por até R$90 mil</a></li>
+                    <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>18 Franquias online para você trabalhar sem sair de casa</a></li>
+                    <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Descubra como cobrar Royalties em uma rede de franquias</a></li>
+                    <li className={styles.changeColor} style={{paddingBottom: '20px', listStyleType: 'none'}}><a>Franquias baratas: mais de 100 dicas por até R$90 mil</a></li>
                     </ul>
                 </div>
             </div>
@@ -104,8 +105,8 @@ const Section5 = () => {
 
             <div style={{float: 'right', paddingRight: '180px', paddingTop: '60px'}}>
                 <p>Aplicativo Mobile</p>
-                <img src={GooglePlayBtn} style={{width: '210px', marginRight: '50px', borderRadius: '10px', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white'}}/>
-                <img src={AppleStoreBtn} style={{width: '220px', borderRadius: '10px', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white'}}/>
+                <a><img src={GooglePlayBtn} style={{width: '210px', marginRight: '50px', borderRadius: '10px', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white'}}/></a>
+                <a><img src={AppleStoreBtn} style={{width: '220px', borderRadius: '10px', borderStyle: 'solid', borderWidth: '1px', borderColor: 'white'}}/></a>
 
             </div>
                 <hr color='grey' style={{clear: 'both', width: '1100px'}}/>
