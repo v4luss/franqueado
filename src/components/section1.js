189051f418f1c3982e72ee050f965f6e289f45be
v4luss <v4luss@gmail.com>
2021-01-27 19:38:35 -0300

    addedIconsAndTextFormating

--- src/components/section1.js
+++ src/components/section1.js
@@ -17,6 +17,7 @@ import {
 } from 'mdbreact'
 import { Link } from 'gatsby'
 import Banner1 from '../images/banner.png'
+import styles from '../styles/section1.module.css'
 
 
 const Section1 = () => {
@@ -24,14 +25,16 @@ const Section1 = () => {
       <MDBModalBody style={{backgroundColor: "#316DE8"}}>
           <div style={{textAlign: "center", color: "white"}}>
             <h2>Promova inovação e getão inteligente com</h2>
-            <h2><b>O Melhor Sistema Para Franquias do Brasil</b></h2>
+            <h2><b style={{fontWeight: 'bold'}}>O Melhor Sistema Para Franquias do Brasil</b></h2>
             <h6>Mantenha seus franqueados engajados e impulsione a expansão da sua<br/>franquia com a Central do Franqueado.</h6>
             </div>
           <img src={Banner1} style={{marginRight: "auto", marginLeft: "auto", display: "block"}}/>
-      <div style={{color: "white", textAlign: "center"}}>
-          <span style={{paddingRight: "70px", paddingLeft: "70px"}}>Comunicação efetiva</span>
-          <span style={{paddingRight: "70px", paddingLeft: "70px"}}>Operação centralizada</span>
-          <span style={{paddingRight: "70px", paddingLeft: "70px"}}>Padronização inteligente</span>
+      <div style={{color: "white", paddingBottom: '80px', fontSize: '20px', marginLeft: '130px'}}>
+        <ul>
+          <li className={styles.item1} style={{paddingRight: "70px", marginLeft: "50px", paddingLeft: '50px', float: 'left'}}><span>Comunicação efetiva</span></li>
+          <li className={styles.item2} style={{paddingRight: "70px", marginLeft: "50px", paddingLeft: '50px', float: 'left'}}>Operação centralizada</li>
+          <li className={styles.item3} style={{paddingRight: "70px", marginLeft: "50px", paddingLeft: '50px', float: 'left'}}>Padronização inteligente</li>
+        </ul>
       </div>
       
       
